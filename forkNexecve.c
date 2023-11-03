72f42b57428b1cf62279c42b2296e0ecf6334ee2
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-11-03 13:35:55 +0530

    fixed fork n execve

--- forkNexecve.c
+++ forkNexecve.c
@@ -1,13 +1,10 @@
-// ThIs ProGRam NeEdS ComMaND liNE InpUtS tO RuN
-// Implement the C program in which main program accepts an integer array. Main program uses the FORK system call to create a new process called a child process. Parent process sorts an integer array and passes the sorted array to child process through the command line arguments of EXECVE system call. The child process uses EXECVE system call to load new program that uses this sorted array for performing the binary search to search the particular item in the array.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-void bubble_sort(int a[50], int n)
+void bubble_sort(int a[], int n)
 {
     int i, j, temp;
     for (i = n - 1; i > 0; i--)
@@ -27,49 +24,65 @@ void bubble_sort(int a[50], int n)
     {
         printf("%d ", a[i]);
     }
+    printf("\n");
+}
+
+int binarySearch(int arr[], int size, int key)
+{
+    int left = 0;
+    int right = size - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key)
+        {
+            printf("Element found");
+            return 0;
+        }
+        if (arr[mid] < key)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    printf("Element not found");
+    return -1;
 }
 
 int main(int argc, char *argv[])
 {
-    int i, j = 2, l, k;
+    int size, key;
 
-    int n = argc - 2;
-    int a[n];
-    char *args[n + 1];
-    args[0] = argv[1];
+    printf("Enter the number of elements to be sorted: ");
+    scanf("%d", &size);
 
-    for (i = 1; i <= n; i++, j++)
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    printf("Enter %d elements: ", size);
+    for (int i = 0; i < size; i++)
     {
-        args[i] = argv[j];
+        scanf("%d", &arr[i]);
     }
-    args[i] = (char *)0;
+    printf("Enter the key to be searched: ");
+    scanf("%d", &key);
 
-    for (l = 0, i = 1; i <= n; i++, l++)
-    {
-        a[l] = atoi(args[i]);
-    }
-    bubble_sort(a, n);
-
-    for (l = 0, i = 1; i <= n; i++, l++)
-    {
-        sprintf(args[i], "%d", a[l]);
-    }
-
-    pid_t pid;
-    pid = fork();
-
+    bubble_sort(arr, size);
+    pid_t pid = fork();
     if (pid == 0)
     {
-        printf("\nI am Child Process,my pid is %d \n", getpid());
-        printf("I am Parent Process,my pid is %d \n", getppid());
-        execve(argv[1], args, NULL);
+        execve(argv[1], argv + 2, NULL);
+
+        binarySearch(arr, size, key);
+        exit(1);
     }
     else
     {
-        k = wait(0);
-        printf("\nI am Parent Process,my pid is %d \n", getpid());
-        printf("I am Parent's Parent Process,my pid is %d \n\n", getppid());
+        int status;
+        wait(&status);
     }
-
+    free(arr);
     return 0;
 }
